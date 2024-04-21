4468a7715240aeffafd63efa7e5f0ee6f2263d92
Wamm K. D <jaft.r@outlook.com>
2024-04-21 15:21:12 -0500

    And the Joint.
    In my original code, I had schemas for either ItemB or ItemC so kind
    of reflecting that, in case that's causing issues.

---
+++ lib/testing/items/joint.ex
@@ -0,0 +1,14 @@
+defmodule Testing.Items.Joint do
+  use    Ecto.Schema;
+  import Ecto.Changeset;
+  alias  Testing.Repo;
+
+  @primary_key {:item_joint_id, :id, autogenerate: true}
+  schema "item_joint" do
+    belongs_to(:item_b, Testing.Items.B, foreign_key: :item_b_id, references: :item_b_id);
+  end
+
+  def changeset(%Testing.Items.B{} = b) do
+    %Testing.Items.B{} |> cast(%{}, []) |> put_assoc(:item_b, b);
+  end
+end
