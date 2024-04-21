5e49a50cf15da5da38021c8b490c7a52fe7828e2
Wamm K. D <jaft.r@outlook.com>
2024-04-21 14:27:51 -0500

    And setup the mapping.

--- lib/testing/items/a.ex
+++ lib/testing/items/a.ex
@@ -6,6 +6,10 @@ defmodule Testing.Items.A do
     field(:i_dunno,   :string);
     field(:smth_else, :integer);
 
+    many_to_many(:item_bs,                         Testing.Items.B,
+                 join_through: "item_a_to_item_b", join_keys: [item_a_id: :item_a_id,
+                                                               item_b_id: :item_b_id]);
+
     timestamps();
   end
 end
