235678e57fc01958d96a618431fb1ff864d550e1
Wamm K. D <jaft.r@outlook.com>
2024-04-21 15:19:39 -0500

    Create our joint table; referenced rows can be ItemB or ItemC.
    I had a constraint, in the original table, that only allowed either
    column to be ~NULL~ but not both or neither but let's keep things
    simple, first, to see if the issue lies in the constraint or elsewhere.

--- priv/repo/migrations/20240421195643_create_item_a_to_item_joint.exs
+++ priv/repo/migrations/20240421195643_create_item_a_to_item_joint.exs
@@ -3,15 +3,15 @@ defmodule Testing.Repo.Migrations.CreateItemAToItemJoint do
 
   def change do
     create table(:item_a_to_item_joint, primary_key: false) do
-      add(:item_a_id, references(:item_a,
-                                 column:    :item_a_id,
-                                 on_delete: :delete_all), primary_key: true);
-      add(:item_b_id, references(:item_b,
-                                 column:    :item_b_id,
-                                 on_delete: :delete_all), primary_key: true);
+      add(:item_a_id,     references(:item_a,
+                                     column:    :item_a_id,
+                                     on_delete: :delete_all), primary_key: true);
+      add(:item_joint_id, references(:item_join,
+                                     column:    :item_join_id,
+                                     on_delete: :delete_all), primary_key: true);
     end
 
-    create(index(:item_a_to_item_b, [:item_a_id]));
-    create(index(:item_a_to_item_b, [:item_b_id]));
+    create(index(:item_a_to_item_joint, [:item_a_id]));
+    create(index(:item_a_to_item_joint, [:item_joint_id]));
   end
 end
