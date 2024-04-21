85b8a8be72f748b49a05e6aefba4a4b4b5cd6ca7
Wamm K. D <jaft.r@outlook.com>
2024-04-21 14:40:33 -0500

    Aaaand set some data up.

--- lib/testing/main_event.ex
+++ lib/testing/main_event.ex
@@ -4,6 +4,16 @@ defmodule Testing.MainEvent do
   """
 
   import Ecto.Query, warn: false;
+  alias  Testing.Repo;
   alias  Testing.Items.A;
   alias  Testing.Items.B;
+
+  def setup() do
+    b1 = %B{} |> B.changeset(%{nonsense: "wut1"}) |> Repo.insert!();
+    b2 = %B{} |> B.changeset(%{nonsense: "wut2"}) |> Repo.insert!();
+    b3 = %B{} |> B.changeset(%{nonsense: "wut3"}) |> Repo.insert!();
+
+    %A{} |> A.changeset(%{i_dunno: "I love Elixir!", smth_else: 42}, [b1, b2, b3])
+         |> Repo.insert!();
+  end
 end
